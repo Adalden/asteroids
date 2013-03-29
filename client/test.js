7e0ee184f9e273e3fd3da0bbe52622678bc080f7
numso <dosmun@gmail.com>
2013-03-28 19:27:28 -0600

    yay for moving asteroids

--- client/test.js
+++ client/test.js
@@ -1,7 +1,7 @@
 var renderer
   , camera
   , scene
-  , mesh;
+  , meshes = [];
 
 exports.init = initialize;
 exports.start = animate;
@@ -40,7 +40,8 @@ function initialize() {
   // attach the render-supplied DOM element
   $container.append(renderer.domElement);
 
-  addModel('models/asteroid.js', 'models/asteroid.jpg');
+  for (var i = 0; i < 5; ++i)
+    addModel('models/asteroid.js', 'models/asteroid.jpg');
 
   // create a point light
   var pointLight = new THREE.PointLight(0xFFFFFF);
@@ -60,14 +61,27 @@ function addModel(_model, meshTexture) {
   loader.load(_model, function (geometry, materials) {
     // materials[0].morphTargets = true;
     // material.shading = THREE.FlatShading;
-    mesh = new THREE.Mesh(geometry, material); // new THREE.MeshFaceMaterial(materials)
-    mesh.position.x = mesh.position.y = mesh.position.z = 0;
+    var mesh = new THREE.Mesh(geometry, material); // new THREE.MeshFaceMaterial(materials)
+    mesh.position.z = 0;
+    mesh.position.x = Math.random() * 180 - 90;
+    mesh.position.y = Math.random() * 180 - 90;
     mesh.rotation.x = mesh.rotation.y = mesh.rotation.z = 0;
-    mesh.scale.x    = mesh.scale.y    = mesh.scale.z    = 40;
+    mesh.scale.x    = mesh.scale.y    = mesh.scale.z    = 20;
     mesh.matrixAutoUpdate = false;
     mesh.updateMatrix();
     scene.add(mesh);
+    meshes.push(createAsteroid(mesh));
   });
+}
+
+function createAsteroid(mesh) {
+  return {
+    rotX: (Math.random() - .5) / 50,
+    rotY: (Math.random() - .5) / 50,
+    dx: (Math.random() - .5) / 2,
+    dy: (Math.random() - .5) / 2,
+    mesh: mesh
+  };
 }
 
 function animate() {
@@ -76,10 +90,36 @@ function animate() {
 }
 
 function render() {
-  if (mesh) {
-    mesh.rotation.y += .01;
-    mesh.rotation.x += .005;
-    mesh.updateMatrix();
+  for (var i = 0; i < meshes.length; ++i) {
+    var a = meshes[i];
+
+    a.mesh.position.x += a.dx;
+    a.mesh.position.y += a.dy;
+
+    if (a.mesh.position.x > 100) {
+      a.mesh.position.x = 100;
+      a.dx *= -1;
+    }
+
+    if (a.mesh.position.x < -100) {
+      a.mesh.position.x = -100;
+      a.dx *= -1;
+    }
+
+    if (a.mesh.position.y > 100) {
+      a.mesh.position.y = 100;
+      a.dy *= -1;
+    }
+
+    if (a.mesh.position.y < -100) {
+      a.mesh.position.y = -100;
+      a.dy *= -1;
+    }
+
+    a.mesh.rotation.z += a.rotX;
+    a.mesh.rotation.x += a.rotY;
+
+    a.mesh.updateMatrix();
   }
 
   renderer.render(scene, camera);
